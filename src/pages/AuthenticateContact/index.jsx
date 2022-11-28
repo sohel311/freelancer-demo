c65ae39a95ceb581612beffb1e7de351f30127d5
sohel.chhipa <sohel.chhipa@artoon.in>
2022-11-29 01:31:59 +0530

    fix: bug

--- src/pages/AuthenticateContact/index.jsx
+++ src/pages/AuthenticateContact/index.jsx
@@ -6,30 +6,35 @@ import CustomModal from "../../components/CustomModal";
 const AuthenticateContact = ({ handleNavigate }) => {
   return (
     <CustomModal overlayClass={"responsive-overlay"} className="signup-pop">
-    <div className="otp">
-      <div className="otp_left">
-        <div className="otp_left_title">Email Confirmation</div>
-        <div className="otp_left_sub-title">
-          Please verify the given abc@gmail.com email id
-        </div>
-        <form className="otp_left_form-wrapper">
-          <div className="otp_left_form-wrapper_form-item" >
-            <label>6 Digit OTP</label>
-            <OtpInput
-              className="otp_left_form-wrapper_form-item_otp"
-              numInputs={6}
-              separator={<span className="separator"></span>}
-            />
+      <div className="otp">
+        <div className={"otp_left_wrapper"}>
+          <div className="otp_left">
+            <div className="otp_left_title">Email Confirmation</div>
+            <div className="otp_left_sub-title">
+              Please verify the given abc@gmail.com email id
+            </div>
+            <form className="otp_left_form-wrapper">
+              <div className="otp_left_form-wrapper_form-item">
+                <label>6 Digit OTP</label>
+                <OtpInput
+                  className="otp_left_form-wrapper_form-item_otp"
+                  numInputs={6}
+                  separator={<span className="separator"></span>}
+                />
+              </div>
+              <button
+                className="otp_left_form-wrapper_submit-btn"
+                type="submit"
+              >
+                Continue
+              </button>
+            </form>
           </div>
-          <button className="otp_left_form-wrapper_submit-btn" type="submit">
-            Continue
-          </button>
-        </form>
+        </div>
+        <div className="otp_right">
+          <img src={SignUpBg} />
+        </div>
       </div>
-      <div className="otp_right">
-        <img src={SignUpBg} />
-      </div>
-    </div>
     </CustomModal>
   );
 };
